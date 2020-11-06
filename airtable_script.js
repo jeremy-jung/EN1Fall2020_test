081487e35a3423aecfe7670f7b841dca65f7484f
Ethan Danahy <edanahy@gmail.com>
2020-11-05 22:09:39 -0500

    Moved service dock init function and changed form to textinput type

--- airtable_script.js
+++ airtable_script.js
@@ -1,13 +1,13 @@
 
 /*
 Airtable_script.js
-Edited 11-4-20 
+Edited 11-4-20
 Ethan Danahy, Teddy Robbins, Jeremy Jung
 */
 
-var airtableElement = document.getElementById("service_airtable");
-airtableElement.init();
-var my_airtable = airtableElement.getService();
+// global variables for interacting with Service Dock's Airtable Service
+var airtableElement;
+var my_airtable;
 
 function updateOnAirtable(key, newValue) {
     /* check if given key already exists on Airtable*/
@@ -36,7 +36,7 @@ function button_function(elem) {
     // retrieve key & value pairs from buttons' attributes
     var key = elem.getAttribute('airtable_value');
     var newValue = elem.innerHTML;
-    
+
     // update or create Name & Value pair on Airtable
     updateOnAirtable(key, newValue);
 
@@ -78,9 +78,9 @@ function setup() {
     for (i=0; i<d.length; i++) {
         d[i].onclick = function () { range_function(this); }
     }
-    d = document.querySelectorAll("form");
+    d = document.querySelectorAll("form[type=textinput]");
     for (i=0; i<d.length; i++) {
-        
+
         var current_form = d[i];
         var text_entry = d[i].elements[0];
         var submit_button = d[i].elements[1];
@@ -91,5 +91,10 @@ function setup() {
 }
 
 window.addEventListener('load', function () {
-        setup();
+    // setup the onclick listeners for the interactive webpage elements
+    setup();
+    // setup the ServiceDock
+    airtableElement = document.getElementById("service_airtable");
+    airtableElement.init();
+    my_airtable = airtableElement.getService();
 });
